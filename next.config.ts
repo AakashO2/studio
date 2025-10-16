2698aeffea5e6d94c7d7971397678faf281735d0
17-DCST-AAKASH GHOSH <ag4198720@gmail.com>
2025-10-16 14:18:01 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -30,10 +30,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    // This is to allow cross-origin requests in development.
-    allowedDevOrigins: ['**'],
-  },
 };
 
 export default nextConfig;
